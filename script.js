941fb83a140971f7d0b619db133053a0d07ce3bc
Bojan227 <80133862+Bojan227@users.noreply.github.com>
2022-01-19 11:46:38 +0100

    Update script.js
    Added checkwin and isDraw functions

--- script.js
+++ script.js
@@ -8,22 +8,42 @@ const createPlayer = (name, mark) => ({
         mark,
 });
 const game = () => {
-        const gameBoard = [];
+        const gameBoard = ['', '', '', '', '', '', '', '', ''];
         const player1 = createPlayer('Gandalf', 'x');
         const player2 = createPlayer('Sauron', 'o');
         return { gameBoard, player1, player2,};
 };
 
+const winCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+let currentPlayer = game().player1.mark
+const result = game().gameBoard
 
-
-function checkWin(){
-    
+function checkWin() {
+    return winCombinations.find(combination =>{
+    return combination.every((index)=> result[index] === currentPlayer)
+    }) || false;
 }
+function isDraw(){
+    return result.every(el => {
+        return el === 'x' || el === 'o'
+    })
+}    
+
 
 function makeGrid(){
     for(let i = 0; i < 9; i++){
         let cell = document.createElement('div');
         cell.classList.add('el')
+        cell.setAttribute('id', i)
         if(i === 0 || i === 3 || i === 6){
             cell.classList.add('not-left')
         } 
@@ -33,30 +53,32 @@ function makeGrid(){
         gameContainer.appendChild(cell)
     }
 }
-let currentPlayer = game().player1.name
-let result = game().gameBoard
+
+function changeTurn(){
+    if(currentPlayer === game().player1.mark){
+        currentPlayer = game().player2.mark;
+    }else {
+        currentPlayer = game().player1.mark;
+    }
+}
+
 gameContainer.addEventListener('click', (e)=>{
     
+    const index = e.target.id
     if(e.target.className === 'container'){return}
     if(e.target.innerText === 'x' || e.target.innerText === 'o'){return}
-    if(currentPlayer === game().player1.name){
+    if(currentPlayer === game().player1.mark){
         e.target.innerText += game().player1.mark
-        result.push(game().player1.mark)
+        result[index] = game().player1.mark
+        console.log(checkWin())
         console.log(result)
-        changeTurn()
+        
     }else{
         e.target.innerText += game().player2.mark
-        result.push(game().player2.mark)
-        console.log(result)
-        changeTurn()
+        result[index] = game().player2.mark
+        console.log(checkWin())
     }
 })
 
-function changeTurn(){
-    if(currentPlayer === game().player1.name){
-        currentPlayer = game().player2.name;
-    }else {
-        currentPlayer = game().player1.name;
-    }
-}
+makeGrid()
 
